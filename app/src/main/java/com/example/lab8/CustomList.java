773c8cc4838bbdee2c524e080861679a7d585a52
Shubham Saharan <shubhamsaharan@hotmail.com>
2024-03-08 11:52:43 -0700

    Created Unit test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 public class CustomList extends ArrayAdapter<City> {
